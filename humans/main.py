3e4ec85e3139e9e47a814ef546c4dcb1a9583d85
Azizbek0307 <azizbekmahumudjonov@gmail.com>
2024-09-28 18:24:27 +0500

    yugudi printi qoshildi

--- humans/main.py
+++ humans/main.py
@@ -1,10 +1,11 @@
-# class Odam ():
-#     def __init__(self,ism):
-#         self.first_name = ism
-#     def metod (self):
-#         print( f"Salom {self.first_name}")
+class Odam ():
+    def __init__(self,ism):
+        self.first_name = ism
+    def metod (self):
+        print( f"Salom {self.first_name}")
+        print( f" {self.first_name} , sen yugrding")
 
-# Odamcha = Odam(input('ismni kiritung :'))
+Odamcha = Odam(input('ismni kiritung :'))
 
-# Odamcha.metod()
+Odamcha.metod()
 
